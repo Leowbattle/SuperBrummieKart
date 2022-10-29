f9476a87b9f46796fed4c83eccc7f8b7fafaed3c
Leo Battle <leowbattle@gmail.com>
2022-10-29 13:33:30 +0100

    I hope the camera vectors are correct

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+// Note to self: In this program +Z axis is up
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -27,6 +29,14 @@ typedef struct vec3 {
 	float z;
 } vec3;
 
+vec3 vec3_cross(vec3 a, vec3 b) {
+	return (vec3){
+		a.y * b.z - a.z * b.y,
+		a.z * b.x - a.x * b.z,
+		a.x * b.y - a.y * b.x
+	};
+}
+
 SDL_Window* window;
 SDL_Renderer* renderer;
 SDL_Texture* frameTexture;
@@ -34,6 +44,44 @@ void* textureData;
 int rowPitch;
 
 bool gameRunning;
+
+typedef struct Camera {
+	vec3 position;
+	float yaw;
+	float pitch;
+	vec2 forward_2d;
+	vec3 forward;
+	vec3 up;
+	vec3 right;
+	float fov_x;
+	float fov_y;
+	float cam_dist;
+} Camera;
+
+void Camera_SetYawPitch(Camera* cam, float yaw, float pitch) {
+	cam->yaw = yaw;
+	cam->pitch = pitch;
+
+	float cy = cosf(yaw);
+	float sy = sinf(yaw);
+	float cp = cosf(pitch);
+	float sp = sinf(pitch);
+
+	cam->forward_2d = (vec2){cy, sy};
+	cam->forward = (vec3){cy * cp, sy * cp, sp};
+	cam->right = (vec3){-sy, cy, 0.0f};
+	cam->up = vec3_cross(cam->forward, cam->right);
+}
+
+void Camera_SetFovX(Camera* cam) {
+
+}
+
+void Camera_SetFovY(Camera* cam) {
+
+}
+
+Camera mainCamera;
 
 int main(int argc, char** argv) {
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -49,6 +97,12 @@ int main(int argc, char** argv) {
 	SDL_RenderSetIntegerScale(renderer, true);
 
 	frameTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB24, SDL_TEXTUREACCESS_STREAMING, GAME_WIDTH, GAME_HEIGHT);
+
+	Camera* cam = &mainCamera;
+	Camera_SetYawPitch(cam, 0, 0);
+	for (int i = 0; i < 10; i++) {
+		Camera_SetYawPitch(cam, cam->yaw, cam->pitch + 0.1f);
+	}
 
     gameRunning = true;
     while (gameRunning) {
